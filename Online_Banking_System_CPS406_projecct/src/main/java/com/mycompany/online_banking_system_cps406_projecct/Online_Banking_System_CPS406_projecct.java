ef18790c721ef01ba9f57abf4676182a29cab64d
dhairyap927 <92445904+dhairyap927@users.noreply.github.com>
2023-03-22 15:05:36 -0400

    Add files via upload

---
+++ Online_Banking_System_CPS406_projecct/src/main/java/com/mycompany/online_banking_system_cps406_projecct/Online_Banking_System_CPS406_projecct.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.online_banking_system_cps406_projecct;
+
+/**
+ *
+ * @author dhairyapatel
+ */
+public class Online_Banking_System_CPS406_projecct {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
